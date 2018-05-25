a2af29e7197f817a5b37ecb971275baf5b2f02b1
Francisco Costa <francisco.costa@unixono.com>
2018-05-25 09:21:05 -0300

    Improve tests readability

--- react-app/src/modules/account/account.test.js
+++ react-app/src/modules/account/account.test.js
@@ -1,25 +1,32 @@
 import * as account from './account.js';
+import naclUtils from 'tweetnacl-util';
 
 afterEach(() => account.clearAccount());
 
-test('creates keypairs from username and password', async () => {
+test('creates keypair and checks it against server side generated keypair', async () => {
   expect.assertions(4);
-  await account.constructor('$/()RUFJ)wuwe84349', 'dnefu384(/·$873hf7g')
-    .then(result => {
-      expect(result).toBeNull();
-      expect(account.getUsername()).toBe('$/()RUFJ)wuwe84349');
-      expect(btoa(String.fromCharCode.apply(null, account.getPublicKey())))
-        .toBe('6FUygceUHlKzGu/5ir4FNkmAtUF07uNeAAdp9+5hcy4=');
-      expect(btoa(String.fromCharCode.apply(null, account.getSecretKey())))
-        .toBe('m4jBZuDiCgcCz94VIWHmeVC9IsXOIavniA2pqeq5Gg0=');
-    });
+  const username = '$/()RUFJ)wuwe84349';
+  const password = 'dnefu384(/·$873hf7g';
+  const result = await account.constructor(username, password);
+  expect(result).toBeNull();
+  expect(account.getUsername()).toBe(username);
+
+  const expectedPublicKey = '6FUygceUHlKzGu/5ir4FNkmAtUF07uNeAAdp9+5hcy4=';
+  const generatedPublicKey = naclUtils.encodeBase64(account.getPublicKey());
+  expect(generatedPublicKey).toBe(expectedPublicKey);
+
+  const expectedSecretKey = 'm4jBZuDiCgcCz94VIWHmeVC9IsXOIavniA2pqeq5Gg0=';
+  const generatedSecretKey = naclUtils.encodeBase64(account.getSecretKey());
+  expect(generatedSecretKey).toBe(expectedSecretKey);
 });
 
-test('encrypt and decrypt a message', () => {
+test('encrypts and decrypts a message', () => {
   expect.assertions(2);
   account._setKeyPair('6FUygceUHlKzGu/5ir4FNkmAtUF07uNeAAdp9+5hcy4=',
-                     'm4jBZuDiCgcCz94VIWHmeVC9IsXOIavniA2pqeq5Gg0=');
-  let encryptedMessage = account.encrypt('sómething with ñ and ü\n!_¡?\'{[^]}}à');
+                      'm4jBZuDiCgcCz94VIWHmeVC9IsXOIavniA2pqeq5Gg0=');
+  const plainText = 'sómething with ñ and ü\n!_¡?\'{[^]}}à';
+  const encryptedMessage = account.encrypt(plainText);
+  const decryptedMessage = account.decrypt(encryptedMessage);
   expect(typeof encryptedMessage).toBe('string');
-  expect(account.decrypt(encryptedMessage)).toBe('sómething with ñ and ü\n!_¡?\'{[^]}}à');
+  expect(decryptedMessage).toBe(plainText);
 });
