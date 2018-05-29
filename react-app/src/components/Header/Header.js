cb948fe6ce67096427157a4f8a1522bfd7a05132
Francisco Costa <francisco.costa@unixono.com>
2018-05-29 16:30:29 -0300

    Add Header component

---
+++ react-app/src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+
+import Logo from 'components/Logo/Logo';
+import './Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <div className="left">
+          {this.props.left}
+        </div>
+        <div className="center">
+          <Logo size="small" />
+        </div>
+        <div className="right">
+          {this.props.right}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ react-app/src/components/Header/Header.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import Header from './Header';
+import ActionIcon from 'components/ActionIcon/ActionIcon';
+
+storiesOf('Header', module)
+  .add('Header with no side icons', () => (
+    <Header />
+  ))
+  .add('Header with left icon', () => (
+    <Header
+      left={<ActionIcon icon="back" />}
+    />))
+  .add('Header with left and right icons', () => (
+    <Header
+      left={<ActionIcon icon="back" />}
+      right={<ActionIcon icon="apply" />}
+    />))
