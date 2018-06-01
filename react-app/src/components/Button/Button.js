294d4f87aa2bacef720a1c479e758eea96f3973b
Francisco Costa <francisco.costa@unixono.com>
2018-06-01 13:38:10 -0300

    Add PropTypes to Button Component and improve its stories with knobs

--- react-app/src/components/Button/Button.js
+++ react-app/src/components/Button/Button.js
@@ -1,6 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Button extends Component {
+  static propTypes = {
+    /**
+     * The label or element the button will show.
+     */
+    children: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.element,
+    ]).isRequired,
+
+    /**
+     * The function to call when this button is clicked.
+     */
+    onClick: PropTypes.func,
+
+    /**
+     * Whether this button is disabled or not.
+     */
+    disabled: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    onClick: () => {},
+    disabled: false,
+  }
+
   render() {
     return (
       <button
--- react-app/src/components/Button/Button.stories.js
+++ react-app/src/components/Button/Button.stories.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, boolean } from '@storybook/addon-knobs/react';
 
 import Button from './Button';
 
-storiesOf('Button', module)
-  .add('Text button', () => (
-    <Button
-      disabled={false}
-      onClick={action('Button clicked')}
-    >
-      Text
-    </Button>
-  ))
-  .add('Disabled text button', () => (
-    <Button
-      disabled={true}
-      onClick={action('Button clicked')}
-    >
-      Text
-    </Button>
-  ))
+const stories = storiesOf('Button', module);
+stories.addDecorator(withKnobs);
+
+stories.add('Text button with onClick prop', () => (
+  <Button
+    disabled={boolean('Disabled', false)}
+    onClick={action('Button clicked')}
+  >
+    Text
+  </Button>
+));
+
+stories.add('Text button without onClick prop', () => (
+  <Button
+    disabled={boolean('Disabled', false)}
+  >
+    Text
+  </Button>
+));
