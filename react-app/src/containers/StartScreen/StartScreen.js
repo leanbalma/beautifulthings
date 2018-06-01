85bffe1450a6478d01025665d9489a770f6197f4
Francisco Costa <francisco.costa@unixono.com>
2018-06-01 14:37:01 -0300

    Improve StartScreen container

--- react-app/src/containers/StartScreen/StartScreen.js
+++ react-app/src/containers/StartScreen/StartScreen.js
@@ -2,35 +2,31 @@ import React, { Component } from 'react';
 
 import './StartScreen.css';
 
-import Logo from 'components/Logo/Logo';
-import Button from 'components/Button/Button';
-import InputText from 'components/InputText/InputText';
+import Logo from 'components/Logo';
+import Button from 'components/Button';
+import InputText from 'components/InputText';
 
 export default class StartScreen extends Component {
   render() {
     return (
       <div className="StartScreen">
-        <header>
-          <Logo size="big" />
-        </header>
-        <main>
-          <div className="col">
-            <InputText
-              type="text"
-              label="Username: "
-              placeholder="Username"
-              onEnter={() => {}}
-            />
-            <InputText
-              type="password"
-              label="Password: "
-              placeholder="Password"
-              onEnter={() => {}}
-            />
-            <Button>Sign In</Button>
-            <Button>Sign Up</Button>
-          </div>
-        </main>
+        <Logo size="big" />
+        <InputText
+          type="text"
+          label="Username: "
+          placeholder="Username"
+          onChange={event => console.log(`Username: ${event}`)}
+          onEnter={() => {console.log('Enter pressed in username input')}}
+        />
+        <InputText
+          type="password"
+          label="Password: "
+          placeholder="Password"
+          onChange={event => console.log(`Password: ${event}`)}
+          onEnter={() => {console.log('Enter pressed in password input')}}
+        />
+        <Button>Sign In</Button>
+        <Button>Sign Up</Button>
       </div>
     );
   }
