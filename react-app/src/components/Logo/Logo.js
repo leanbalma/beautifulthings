a03e478861b89a43e890967e89dcf316caf06874
Francisco Costa <francisco.costa@unixono.com>
2018-06-01 13:55:09 -0300

    Add PropTypes to Logo Component and improve its stories with knobs

--- react-app/src/components/Logo/Logo.js
+++ react-app/src/components/Logo/Logo.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import PlaceholderLogo from './PlaceholderLogo.svg';
 import './Logo.css';
 
+const SMALL = 'small';
+const BIG = 'big';
+
 export default class Logo extends Component {
+  static propTypes = {
+    /**
+     * The size for the logo. Possible values: 'small' or 'big'
+     */
+    size: PropTypes.oneOf([SMALL, BIG]).isRequired,
+  };
+
   render() {
     return (
       <img
--- react-app/src/components/Logo/Logo.stories.js
+++ react-app/src/components/Logo/Logo.stories.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, selectV2 } from '@storybook/addon-knobs/react';
 
 import Logo from './Logo';
 
-storiesOf('Logo', module)
-  .add('Logo big', () => (
-    <Logo size="big" />
-  ))
-  .add('Logo small', () => (
-    <Logo size="small"/>
-  ))
+const stories = storiesOf('Logo', module);
+stories.addDecorator(withKnobs);
+
+const availableSizes = {
+  small: 'small',
+  big: 'big',
+}
+
+const defaultSize = availableSizes.small;
+
+stories.add('Logo', () => (
+  <Logo size={selectV2('Size', availableSizes, defaultSize)} />
+));
