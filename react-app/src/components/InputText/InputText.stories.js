bfc747929c8c70570c3414e8d6cfda26b07b1c64
Francisco Costa <francisco.costa@unixono.com>
2018-06-01 14:19:01 -0300

    Rename InputText.js to index.js

--- react-app/src/components/InputText/InputText.stories.js
+++ react-app/src/components/InputText/InputText.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text } from '@storybook/addon-knobs/react';
 
-import InputText from './InputText';
+import InputText from './';
 
 const stories = storiesOf('InputText', module);
 stories.addDecorator(withKnobs);
