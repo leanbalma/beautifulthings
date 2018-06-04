f13a02595010a76d8b448e1c786b310bd9d9dac6
Francisco Costa <francisco.costa@unixono.com>
2018-06-04 16:48:38 -0300

    Add PropTypes to Header and improve its stories

--- react-app/src/components/Header/index.js
+++ react-app/src/components/Header/index.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
 
 import Logo from 'components/Logo';
 import './index.css';
 
 export default class Header extends Component {
+  static propTypes = {
+    /**
+     * The element (an ActionIcon) that this element will show in the left.
+     */
+    left: PropTypes.element,
+
+    /**
+     * The element (an ActionIcon) that this element will show in the right.
+     */
+    right: PropTypes.element,
+  };
+
+  static defaultProps = {
+    left: null,
+    right: null,
+  }
+
   render() {
     return (
       <div className="Header">
@@ -12,7 +29,7 @@ export default class Header extends Component {
           {this.props.left}
         </div>
         <div className="center">
-          <Logo size="small" />
+          <Logo size={Logo.SMALL} />
         </div>
         <div className="right">
           {this.props.right}
--- react-app/src/components/Header/stories.js
+++ react-app/src/components/Header/stories.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 
 import Header from './';
 import ActionIcon from 'components/ActionIcon';
 
-storiesOf('Header', module)
-  .add('Header with no side icons', () => (
-    <Header />
-  ))
-  .add('Header with left icon', () => (
-    <Header
-      left={<ActionIcon icon="back" />}
-    />))
-  .add('Header with left and right icons', () => (
-    <Header
-      left={<ActionIcon icon="back" />}
-      right={<ActionIcon icon="apply" />}
-    />))
+const stories = storiesOf('Header', module);
+
+stories.add('Header with no side icons', () => (
+  <Header />
+));
+
+stories.add('Header with left icon', () => (
+  <Header left={<ActionIcon icon={ActionIcon.BACK} color="gray" />} />
+));
+
+stories.add('Header with left and right icons', () => (
+  <Header
+    left={<ActionIcon icon={ActionIcon.BACK} color="gray" />}
+    right={<ActionIcon icon={ActionIcon.APPLY} color="green" />}
+  />
+));
