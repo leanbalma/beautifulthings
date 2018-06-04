707afe729a283397a89153546d7a36f28cb4ed76
Francisco Costa <francisco.costa@unixono.com>
2018-06-04 16:21:26 -0300

    Add PropTypes to ActionIcon and improve its stories with knobs

--- react-app/src/components/ActionIcon/index.js
+++ react-app/src/components/ActionIcon/index.js
@@ -1,35 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faTrashAlt, faCheck } from '@fortawesome/fontawesome-free-solid';
 
-const ACTION_ICON_DEFAULT_COLOR = '#aaaaaa';
-
 export default class ActionIcon extends Component {
-  render() {
-    let icon;
+  static BACK = 'back';
+  static REMOVE = 'remove';
+  static APPLY = 'apply';
+
+  static propTypes = {
+    /**
+     * The icon that this element will show.
+     */
+    icon: PropTypes.oneOf([ActionIcon.BACK, ActionIcon.REMOVE, ActionIcon.APPLY]).isRequired,
+
+    /**
+     * The color that the icon will have.
+     */
+    color: PropTypes.string,
+
+    /**
+     * The function to call when this element is clicked.
+     */
+    onClick: PropTypes.func,
+  };
+
+  static defaultProps = {
+    color: '',
+    onClick: () => {},
+  }
+
+  _getIcon = () => {
     switch (this.props.icon) {
-      case 'back':
-        icon=faChevronLeft
-      break;
-      case 'remove':
-        icon=faTrashAlt
-      break;
-      case 'apply':
-        icon=faCheck
-      break;
-      default:
-        // TODO: TBD
-      break;
+      case ActionIcon.BACK:
+        return faChevronLeft
+      case ActionIcon.REMOVE:
+        return faTrashAlt
+      case ActionIcon.APPLY:
+        return faCheck
     }
+  }
+
+  render() {
+    const icon = this._getIcon();
 
     return (
-      <div>
-        <FontAwesomeIcon
-          icon={icon}
-          color={this.props.color || ACTION_ICON_DEFAULT_COLOR}
-          onClick={event => this.props.onClick(event) || null}
-        />
-      </div>
+      <FontAwesomeIcon
+        icon={icon}
+        color={(this.props.color !== '') ? this.props.color : null}
+        onClick={event => this.props.onClick(event)}
+      />
     );
   }
 }
--- react-app/src/components/ActionIcon/stories.js
+++ react-app/src/components/ActionIcon/stories.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, selectV2, text } from '@storybook/addon-knobs/react';
 
 import ActionIcon from './';
 
-storiesOf('ActionIcon', module)
-  .add('ActionIcon back', () => (
-    <ActionIcon
-      icon="back"
-      onClick={action('ActionIcon clicked')}
-    />))
-  .add('ActionIcon remove red', () => (
-    <ActionIcon
-      icon="remove"
-      color="#f33"
-      onClick={action('ActionIcon clicked')}
-    />))
-  .add('ActionIcon save', () => (
-    <ActionIcon
-      icon="apply"
-      onClick={action('ActionIcon clicked')}
-    />))
+const stories = storiesOf('ActionIcon', module);
+stories.addDecorator(withKnobs);
+
+const availableIcons = {
+  back: ActionIcon.BACK,
+  remove: ActionIcon.REMOVE,
+  apply: ActionIcon.APPLY,
+}
+
+stories.add('ActionIcon with onClick prop', () => (
+  <ActionIcon
+    icon={selectV2('Icon', availableIcons, availableIcons.back)}
+    color={text('Color', '')}
+    onClick={action('ActionIcon clicked')}
+  >
+  </ActionIcon>
+));
+
+stories.add('ActionIcon without onClick prop', () => (
+  <ActionIcon
+    icon={selectV2('Icon', availableIcons, availableIcons.back)}
+    color={text('Color', '')}
+  >
+  </ActionIcon>
+));
