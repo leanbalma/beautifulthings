084c30e87bd23c30e7bb4a50ab4467e647883b57
Francisco Costa <francisco.costa@unixono.com>
2018-06-04 18:11:20 -0300

    Improve BaseScreen with PropTypes and change it to a PureComponent

--- react-app/src/containers/BaseScreen/index.js
+++ react-app/src/containers/BaseScreen/index.js
@@ -1,8 +1,28 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 
 import './index.css';
 
-export default class BaseScreen extends Component {
+export default class BaseScreen extends PureComponent {
+  static propTypes = {
+    /**
+     * The element to use as a header (expect a Header).
+     */
+    header: PropTypes.element.isRequired,
+
+    /**
+     * The element with the main content of the screen.
+     */
+    main: PropTypes.element.isRequired,
+
+    /**
+     * The element to use as a footer.
+     */
+    footer: PropTypes.element,
+  };
+
+  static defaultProps = { footer: null }
+
   render() {
     return (
       <div className="BaseScreen">
