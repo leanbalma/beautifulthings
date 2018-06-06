1b189cf8bfc35c3aa41465592fcfc9d54990742a
Francisco Costa <francisco.costa@unixono.com>
2018-06-06 11:52:57 -0300

    Improve StartScreen container
    Add states for username and password inputs values.
    Add support to handle onEnter event in username input.
    Replace arrow functions with named functions for props elements.

--- react-app/src/containers/StartScreen/index.js
+++ react-app/src/containers/StartScreen/index.js
@@ -7,6 +7,27 @@ import Button from 'components/Button';
 import InputText from 'components/InputText';
 
 export default class StartScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  componentDidMount() { this._usernameInputRef.focus(); }
+
+  _setUsernameInputRef = element => this._usernameInputRef = element;
+  _setPasswordInputRef = element => this._passwordInputRef = element;
+
+  _handleUsernameChange = username => this.setState({ username });
+  _handlePasswordChange = password => this.setState({ password });
+
+  _handleUsernameEnter = () => this._passwordInputRef.focus();
+
+  _isUsernameAndPasswordValid = () =>
+    (this.state.username.length > 0 && this.state.password.length > 0) ? true : false;
+
   render() {
     return (
       <div className={styles.container}>
@@ -15,17 +36,18 @@ export default class StartScreen extends PureComponent {
           type={InputText.TEXT}
           label="Username: "
           placeholder="Username"
-          onChange={event => console.log(`Username: ${event}`)}
-          onEnter={() => {console.log('Enter pressed in username input')}}
+          onChange={this._handleUsernameChange}
+          onEnter={this._handleUsernameEnter}
+          ref={this._setUsernameInputRef}
         />
         <InputText
           type={InputText.PASSWORD}
           label="Password: "
           placeholder="Password"
-          onChange={event => console.log(`Password: ${event}`)}
-          onEnter={() => {console.log('Enter pressed in password input')}}
+          onChange={this._handlePasswordChange}
+          ref={this._setPasswordInputRef}
         />
-        <Button>Sign In</Button>
+        <Button disabled={!this._isUsernameAndPasswordValid()}>Sign In</Button>
         <Button>Sign Up</Button>
       </div>
     );
