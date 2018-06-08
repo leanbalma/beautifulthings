ce3f5c20e660fdb90a824ee6d7b4dcbe2561745e
Francisco Costa <francisco.costa@unixono.com>
2018-06-08 09:47:44 -0300

    Rebase branch to master, add details route and fix some minor code-styling errors

--- react-app/src/containers/AppRouter/index.js
+++ react-app/src/containers/AppRouter/index.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Redirect } from 'react-router-dom';
 
 import StartScreen from 'containers/StartScreen';
+import DetailScreen from 'containers/DetailScreen';
 
 class AppRouter extends Component {
   render() {
     return (
       <HashRouter>
         <div>
-          <Route exact path="/" component={null} />
+          <Redirect exact path="/" to="/start" />
           <Route exact path="/start" component={StartScreen} />
+          <Route exact path="/details" component={DetailScreen} />
         </div>
       </HashRouter>
     );
