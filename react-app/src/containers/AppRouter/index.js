439e9c1deb6b9a7afcda802e56aeeaf4796b9f10
Francisco Costa <francisco.costa@unixono.com>
2018-06-08 14:50:02 -0300

    Include DetailScreen in AppRouter

--- react-app/src/containers/AppRouter/index.js
+++ react-app/src/containers/AppRouter/index.js
@@ -5,13 +5,22 @@ import StartScreen from 'containers/StartScreen';
 import DetailScreen from 'containers/DetailScreen';
 
 class AppRouter extends Component {
+  _detailScreen = () =>
+    <DetailScreen
+      date="2018-01-01"
+      text="demo"
+      onBack={() => console.log("Back pressed")}
+      onRemove={() => console.log("Remove pressed")}
+      onEdit={() => console.log("Edit pressed")}
+    />;
+
   render() {
     return (
       <HashRouter>
         <div>
           <Redirect exact path="/" to="/start" />
           <Route exact path="/start" component={StartScreen} />
-          <Route exact path="/details" component={DetailScreen} />
+          <Route exact path="/details" render={this._detailScreen} />
         </div>
       </HashRouter>
     );
