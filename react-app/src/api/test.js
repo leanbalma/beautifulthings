e0ba2263eb9cd3220c3fda3da520192a95b4d33f
Francisco Costa <francisco.costa@unixono.com>
2018-07-05 17:57:55 -0300

    Improve api tests by global.fetch

--- react-app/src/api/test.js
+++ react-app/src/api/test.js
@@ -4,51 +4,79 @@ import naclUtils from 'tweetnacl-util';
 import api from 'api';
 import { createEntry } from 'utils/entry';
 
-const entry = createEntry('2018-01-01', 'A simple entry to test the API');
+// Account data
+const username = 'username';
+const base64EncodedPublicKey = 'ZYw1z/ZfDN9x4dlQ4H9VXdZWNKdamCG7WbxB9LMytnU=';
+const base64EncodedSecretKey = 'g2ukZjfSZW+Ag/Y2L3t3soV5djM5eyYaVuV7oWdGm0I=';
+const keyPair = {
+  publicKey: naclUtils.decodeBase64(base64EncodedPublicKey),
+  secretKey: naclUtils.decodeBase64(base64EncodedSecretKey),
+};
 
-beforeAll(async () => {
-  const username = 'username';
-  // The computed keyPair assumes { username: 'username', password: 'password' }
-  const publicKeyBase64Encoded = 'ZYw1z/ZfDN9x4dlQ4H9VXdZWNKdamCG7WbxB9LMytnU=';
-  const secretKeyBase64Encoded = 'g2ukZjfSZW+Ag/Y2L3t3soV5djM5eyYaVuV7oWdGm0I=';
-  const keyPair = {
-    publicKey: naclUtils.decodeBase64(publicKeyBase64Encoded),
-    secretKey: naclUtils.decodeBase64(secretKeyBase64Encoded),
-  }
+// Encrypted/decrypted message usign the account
+const encryptedToken = 'gw1U4yWauDaMY4OojNkaeP6AqHOmmq/UMLQvk7bJkzQGhCLbZeV0AQhdq1z0lQkIBUORprg=';
+const decryptedToken = 'Token';
 
-  api.initAccount(username, keyPair);
+const entryDate = '2018-05-01';
+const encryptedEntry = createEntry(entryDate, decryptedToken);
+const decryptedEntry = createEntry(entryDate, decryptedToken);
+
+beforeAll(async () => api.initAccount(username, keyPair));
+
+describe('sign up', () => {
+  test('can sign up with a new account', async () => {
+    global.fetch = jest.fn().mockImplementation(() => Promise.resolve({ ok: true }));
+    const result = await api.signUp();
+    expect(result).toBe(true);
+  });
+
+  test('cannot sign up with an existing account', async () => {
+    global.fetch = jest.fn().mockImplementation(() => Promise.resolve({ ok: false }));
+    const result = await api.signUp();
+    expect(result).toBe(false);
+  });
 });
 
-test('success when singup with an unexisting account', async () => {
-  expect.assertions(1);
-  const result = await api.signUp();
-  expect(result).toBe(true);
+describe('sign in', () => {
+  test('can sign in with valid account', async () => {
+    const serverResponse = Promise.resolve({
+      ok: true,
+      json: () => {
+        return { EncryptedToken: encryptedToken };
+      }
+    });
+    global.fetch = jest.fn().mockImplementation(() => serverResponse);
+    const result = await api.signIn();
+    expect(result).toBe(true);
+  });
+
+  test('cannot sign in with invalid account', async () => {
+    global.fetch = jest.fn().mockImplementation(() => Promise.resolve({ ok: false }));
+    const result = await api.signIn();
+    expect(result).toBe(false);
+  });
 });
 
-test('fails when singup with an existing account', async () => {
-  expect.assertions(1);
-  const result = await api.signUp();
-  expect(result).toBe(false);
-});
+describe('user requests when signed in', () => {
+  test('add a new entry', async () => {
+    global.fetch = jest.fn().mockImplementation(() => Promise.resolve({ ok: true }));
+    const result = await api.addEntry(encryptedEntry);
+    expect(result).toBe(true);
+  });
 
-test('success when signin with an existing account', async() => {
-  expect.assertions(1);
-  const result = await api.signIn();
-  expect(result).toBe(true);
-});
-
-test('success when add a new entry', async() => {
-  expect.assertions(1);
-  const result = await api.addEntry(entry);
-  expect(result).toBe(true);
-});
-
-test('success when get the entry added', async() => {
-  expect.assertions(2);
-  const from = '2018-01-01';
-  const to = '2018-01-02';
-
-  const entries = await api.getEntries(from, to);
-  expect(entries.length).toBe(1);
-  expect(entries[0]).toEqual(entry);
+  test('request entries', async () => {
+    const serverResponse = Promise.resolve({
+      ok: true,
+      json: () => {
+        return [{
+          Date: entryDate,
+          Content: encryptedToken
+        }];
+      }
+    });
+    global.fetch = jest.fn().mockImplementation(() => serverResponse);
+    const result = await api.getEntries('2018-01-01', '2019-01-01');
+    expect(result).toHaveLength(1);
+    expect(result).toContainEqual(decryptedEntry);
+  });
 });
