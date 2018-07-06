3165bfbcfbf3dc89a06cbfae0c05fe63fe61b238
Francisco Costa <francisco.costa@unixono.com>
2018-07-06 16:26:31 -0300

    Rewrite StartScreen using BaseUserPassScreen

--- react-app/src/containers/StartScreen/index.js
+++ react-app/src/containers/StartScreen/index.js
@@ -1,55 +1,61 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Account from 'account';
+
+import BaseUserPassScreen from 'containers/BaseUserPassScreen';
+import Button from 'components/Button';
 
 import styles from './index.module.scss';
 
-import Logo from 'components/Logo';
-import Button from 'components/Button';
-import InputText from 'components/InputText';
+export default class StartScreen extends React.PureComponent {
+  static propTypes = { onSignUp: PropTypes.func.isRequired }
 
-export default class StartScreen extends PureComponent {
   constructor(props) {
     super(props);
+
     this.state = {
       username: '',
       password: '',
     };
   }
 
-  componentDidMount() { this._usernameInputRef.focus(); }
-
-  _setUsernameInputRef = element => this._usernameInputRef = element;
-  _setPasswordInputRef = element => this._passwordInputRef = element;
-
   _handleUsernameChange = username => this.setState({ username });
   _handlePasswordChange = password => this.setState({ password });
+  _handlePasswordEnter = () => this._signIn();
 
-  _handleUsernameEnter = () => this._passwordInputRef.focus();
+  _handleSignUp = () => this.props.onSignUp();
+  _handleSignIn = () => this._signIn();
 
-  _isUsernameAndPasswordValid = () =>
-    (this.state.username.length > 0 && this.state.password.length > 0) ? true : false;
+  _signIn() {
+    const {username, password} = this.state;
+
+    if (!Account.validateUserPass(username, password)) return;
+
+    // TODO: Show spinner, create account and signin
+  }
 
   render() {
+    const validUserPass = Account.validateUserPass(this.state.username, this.state.password);
+
     return (
-      <div className={styles.container}>
-        <Logo size={Logo.BIG} />
-        <InputText
-          type={InputText.TEXT}
-          label="Username: "
-          placeholder="Username"
-          onChange={this._handleUsernameChange}
-          onEnter={this._handleUsernameEnter}
-          ref={this._setUsernameInputRef}
-        />
-        <InputText
-          type={InputText.PASSWORD}
-          label="Password: "
-          placeholder="Password"
-          onChange={this._handlePasswordChange}
-          ref={this._setPasswordInputRef}
-        />
-        <Button disabled={!this._isUsernameAndPasswordValid()}>Sign In</Button>
-        <Button>Sign Up</Button>
-      </div>
+      <BaseUserPassScreen
+        onUsernameChange={this._handleUsernameChange}
+        onPasswordChange={this._handlePasswordChange}
+        onPasswordEnter={this._handlePasswordEnter}
+      >
+        <div className={styles.container}>
+          <Button
+            disabled={!validUserPass}
+            onClick={this._handleSignIn}
+          >
+            Sign In
+          </Button>
+          <Button onClick={this._handleSignUp}>
+            Sign Up
+          </Button>
+        </div>
+      </BaseUserPassScreen>
     );
   }
 }
