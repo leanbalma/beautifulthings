d3fbd0038ee3a6dbba751f87b27fa12b40e02e17
Francisco Costa <francisco.costa@unixono.com>
2018-07-26 13:06:08 -0300

    Add styles and refactor StartScreen

--- react-app/src/containers/StartScreen/index.js
+++ react-app/src/containers/StartScreen/index.js
@@ -5,55 +5,134 @@ import Account from 'account';
 
 import BaseUserPassScreen from 'containers/BaseUserPassScreen';
 import Button from 'components/Button';
+import ButtonsModal from 'components/ButtonsModal';
 
 import styles from './index.module.scss';
 
 export default class StartScreen extends React.PureComponent {
-  static propTypes = { onSignUp: PropTypes.func.isRequired }
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: '',
-      password: '',
-    };
+  static propTypes = {
+    onSignUp: PropTypes.func.isRequired,
   }
 
-  _handleChanges = (username, password) => this.setState({ username, password });
-  _handlePasswordEnter = () => this._signIn();
+  state = {
+    username: '',
+    password: '',
+    usernameError: '',
+    passwordError: '',
+    isModalVisible: false,
+  };
+
+  _validateForm() {
+    const { username, password } = this.state;
+
+    return username.length && password.length;
+  }
+
+  _handleUsernameChange = username => {
+    const valid = Account.validateUsername(username);
+
+    if (valid) {
+      this.setState({
+        username,
+        usernameError: '',
+      });
+    } else {
+      this.setState({
+        username: '',
+        usernameError: 'Username is too short',
+      });
+    }
+  }
+
+  _handlePasswordChange = password => {
+    const valid = Account.validatePassword(password);
+
+    if (valid) {
+      this.setState({
+        password,
+        passwordError: '',
+      });
+    } else {
+      this.setState({
+        password: '',
+        passwordError: 'Password is too short',
+      });
+    }
+  }
+
+  _handlePasswordEnter = () => {
+    const validForm = this._validateForm();
+
+    if (validForm) this._signIn();
+  }
+
+  _toggleModalVisibility = () => this.setState({ isModalVisible: !this.state.isModalVisible });
 
   _handleSignUp = () => this.props.onSignUp();
   _handleSignIn = () => this._signIn();
 
-  _signIn() {
-    const {username, password} = this.state;
+  _signIn() { /* TODO: Show spinner, create account and signin or show modal */ }
 
-    if (!Account.validateUserPass(username, password)) return;
+  _getSignInButton() {
+    const validFormDate = this._validateForm();
 
-    // TODO: Show spinner, create account and signin
+    return Button({
+      disabled: !validFormDate,
+      onClick: this._handleSignIn,
+      children: "Sign in",
+    });
+  }
+
+  _getSignUpButton() {
+    return Button({
+      onClick: this._handleSignUp,
+      children: "Sign up",
+    });
+  }
+
+  _getWrongUsernameOrPasswordModal() {
+    const closeModalButton = Button({
+      children: "Try again",
+      onClick: this._toggleModalVisibility,
+      small: true,
+    });
+
+    return ButtonsModal({
+      visible: this.state.isModalVisible,
+      message: "Your username or password is incorrect",
+      primaryButton: closeModalButton,
+    });
   }
 
   render() {
-    const validUserPass = Account.validateUserPass(this.state.username, this.state.password);
+    const { usernameError, passwordError } = this.state;
+    const signInButton = this._getSignInButton();
+    const signUpButton = this._getSignUpButton();
+    const wrongUsernameOrPasswordModal = this._getWrongUsernameOrPasswordModal();
 
     return (
-      <BaseUserPassScreen
-        onChanges={this._handleChanges}
-        onPasswordEnter={this._handlePasswordEnter}
-      >
-        <div className={styles.container}>
-          <Button
-            disabled={!validUserPass}
-            onClick={this._handleSignIn}
-          >
-            Sign In
-          </Button>
-          <Button onClick={this._handleSignUp}>
-            Sign Up
-          </Button>
-        </div>
-      </BaseUserPassScreen>
+      <div>
+        {wrongUsernameOrPasswordModal}
+        <BaseUserPassScreen
+          usernameError={usernameError}
+          passwordError={passwordError}
+          onUsernameChange={this._handleUsernameChange}
+          onPasswordChange={this._handlePasswordChange}
+          onPasswordEnter={this._handlePasswordEnter}
+        >
+          <div className={styles.container}>
+            <div>
+              {signInButton}
+            </div>
+            <div className={styles.createAccountLabelContainer}>
+              - Create your account -
+            </div>
+            <div>
+              {signUpButton}
+            </div>
+          </div>
+        </BaseUserPassScreen>
+      </div>
     );
   }
 }
