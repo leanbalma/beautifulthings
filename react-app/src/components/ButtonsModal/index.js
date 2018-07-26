8cfaf5bc002ea1eb47f8701a45f9cff366ba05d9
Francisco Costa <francisco.costa@unixono.com>
2018-07-26 12:08:23 -0300

    Add styles and refactor ButtonsModal

--- react-app/src/components/ButtonsModal/index.js
+++ react-app/src/components/ButtonsModal/index.js
@@ -1,48 +1,83 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import BaseModal from 'components/BaseModal';
 
 import styles from './index.module.scss';
 
-export default class ButtonsModal extends PureComponent {
-  static propTypes = {
-    /**
-     * Whenever the modal is visible or not
-     */
-    visible: PropTypes.bool.isRequired,
+const ButtonsModal = props => {
+  const {
+    visible,
+    message,
+    primaryButton,
+    secondaryButton,
+  } = props;
 
-    /**
-     * The message that the modal will show
-     */
-    message: PropTypes.string.isRequired,
-
-    /**
-     * The primary button (a Button) that the element will show
-     */
-    primaryButton: PropTypes.element.isRequired,
-
-    /**
-     * The (optional) secondary button (a Button) that the element will show
-     */
-    secondaryButton: PropTypes.element,
-  };
-
-  static defaultProps = { secondaryButton: null }
-
-  render() {
-    return (
-      <BaseModal visible={this.props.visible}>
-        <div className={styles.container}>
+  function _getButtons() {
+    if (!secondaryButton) {
+      return (
+        <div className={styles.buttonsContainer}>
           <div>
-            {this.props.message}
-          </div>
-          <div className={styles.buttonsContainer}>
-            {this.props.secondaryButton}
-            {this.props.primaryButton}
+            {primaryButton}
           </div>
         </div>
-      </BaseModal>
-    );
+      );
+    } else {
+      return (
+        <div className={styles.buttonsContainer}>
+          <div />
+          <div>
+            {primaryButton}
+          </div>
+          <div>
+            {secondaryButton}
+          </div>
+          <div />
+        </div>
+      );
+    }
   }
+
+  const buttons = _getButtons();
+
+  const modalBody = (
+    <div className={styles.container}>
+      <div>
+        {message}
+      </div>
+      <div>
+        {buttons}
+      </div>
+    </div>
+  );
+
+  return BaseModal({
+    visible,
+    children: modalBody,
+  });
+
 }
+
+ButtonsModal.propTypes = {
+  /**
+   * Whenever the modal is visible or not
+   */
+  visible: PropTypes.bool.isRequired,
+
+  /**
+   * The message that the modal will show
+   */
+  message: PropTypes.string.isRequired,
+
+  /**
+   * The primary button (a Button) that the element will show
+   */
+  primaryButton: PropTypes.element.isRequired,
+
+  /**
+   * The (optional) secondary button (a Button) that the element will show
+   */
+  secondaryButton: PropTypes.element,
+};
+
+export default ButtonsModal;
