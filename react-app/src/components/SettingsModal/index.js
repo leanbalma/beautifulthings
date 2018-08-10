36150019908e2273b37fd31cbfad1b15353c53cf
Francisco Costa <francisco.costa@unixono.com>
2018-08-10 12:54:20 -0300

    Add SettingsModal component

---
+++ react-app/src/components/SettingsModal/index.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import ActionIcon from 'components/ActionIcon';
+import BaseModal from 'components/BaseModal';
+import Logo from 'components/Logo';
+import RadioButton from 'components/RadioButton';
+import SettingsOptionLabel from 'components/SettingsOptionLabel';
+
+import styles from './index.module.scss';
+
+export default class SettingsModal extends React.PureComponent {
+  static propTypes = {
+    /**
+     * The username to display
+     */
+    username: PropTypes.string.isRequired,
+
+    /**
+     * The function to call when hide icon is tapped
+     */
+    onHide: PropTypes.func.isRequired,
+
+    /**
+     * The function to call when logout option is tapped
+     */
+    onSignOut: PropTypes.func.isRequired,
+  }
+
+  state = {
+    visible: false,
+    daily: true,
+  };
+
+  _setDailyNotifications = () => this.setState({ daily: true });
+  _setWeeklyNotifications = () => this.setState({ daily: false });
+
+  _handleHide = () => {
+    this.setState({ visible: false });
+    this.props.onHide(this.state.daily);
+  }
+
+  _renderHeader() {
+    return (
+      <div className={styles.headerContainer}>
+        <div className={styles.hideIcon}>
+          <ActionIcon
+            icon={ActionIcon.HIDE}
+            onClick={this._handleHide}
+          />
+        </div>
+        <div className={styles.usernameContainer}>
+          <div className={styles.calendarIcon} />
+          <div className={styles.username}>
+            {this.props.username}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  _renderMain() {
+    const { daily } = this.state;
+
+    const notifications = <SettingsOptionLabel
+      icon={SettingsOptionLabel.NOTIFICATION}
+      text={'Notifications'}
+    />
+
+    const signOut = <SettingsOptionLabel
+      icon={SettingsOptionLabel.SIGNOUT}
+      text={'Sign out'}
+      onClick={this.props.onSignOut}
+    />
+
+    const radioButtonDaily = <RadioButton
+      label={"Daily"}
+      selected={daily}
+      onClick={this._setDailyNotifications}
+    />
+
+    const radioButtonWeekly = <RadioButton
+      label={"Weekly"}
+      selected={!daily}
+      onClick={this._setWeeklyNotifications}
+    />
+
+    return (
+      <div>
+        <div>
+          {notifications}
+        </div>
+        <div className={styles.radioButtonsContainer}>
+          <div>
+            {radioButtonDaily}
+          </div>
+          <div>
+            {radioButtonWeekly}
+          </div>
+        </div>
+        <div>
+          {signOut}
+        </div>
+      </div>
+    );
+  }
+
+  show(daily) {
+    this.setState({
+      visible: true,
+      daily,
+    });
+  }
+
+  render() {
+    const header = this._renderHeader();
+    const main = this._renderMain();
+
+    return (
+      <BaseModal
+        visible={this.state.visible}
+        onDismiss={this._handleHide}
+        leftModal
+      >
+        <div className={styles.container}>
+          <div className={styles.header}>
+            {header}
+          </div>
+          <div className={styles.main}>
+            {main}
+          </div>
+          <div className={styles.footer}>
+            <Logo />
+          </div>
+        </div>
+      </BaseModal>
+    );
+  }
+}
---
+++ react-app/src/components/SettingsModal/stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text } from '@storybook/addon-knobs/react';
+
+import SettingsModal from './';
+
+const _setModalRef = element => element.show(true);
+
+const stories = storiesOf('SettingsModal', module);
+stories.addDecorator(withKnobs);
+
+stories.add('SettingsModal', () => {
+  return <SettingsModal
+    username={text('Username', 'username')}
+    onHide={action('Hide pressed')}
+    onSignOut={action('SignOut pressed')}
+    ref={_setModalRef}
+  />
+});
