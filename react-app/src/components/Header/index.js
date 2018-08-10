05e2152623b54c248c7f1cabff608b5ecf849e7c
Francisco Costa <francisco.costa@unixono.com>
2018-08-10 15:19:03 -0300

    Add styles and refactor Header
    A new prop whiteLogo was added to allow the use of the Header with
    the white logo of the app if needed (according to the design, it will
    be used in the EditScreen).

--- react-app/src/components/Header/index.js
+++ react-app/src/components/Header/index.js
@@ -1,40 +1,46 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import Logo from 'components/Logo';
 import styles from './index.module.scss';
 
-export default class Header extends PureComponent {
-  static propTypes = {
-    /**
-     * The element (an ActionIcon) that this element will show in the left.
-     */
-    left: PropTypes.element,
-
-    /**
-     * The element (an ActionIcon) that this element will show in the right.
-     */
-    right: PropTypes.element,
-  };
-
-  static defaultProps = {
-    left: null,
-    right: null,
-  }
-
-  render() {
-    return (
-      <div className={styles.container}>
-        <div className={styles.left}>
-          {this.props.left}
-        </div>
-        <div className={styles.center}>
-          <Logo size={Logo.SMALL} />
-        </div>
-        <div className={styles.right}>
-          {this.props.right}
-        </div>
+const Header = ({ left, right, whiteLogo }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        {left}
       </div>
-    );
-  }
+      <div className={styles.center}>
+        <Logo white={whiteLogo} />
+      </div>
+      <div className={styles.right}>
+        {right}
+      </div>
+    </div>
+  );
 }
+
+Header.propTypes = {
+  /**
+   * The element (an ActionIcon) that this element will show in the left.
+   */
+  left: PropTypes.element,
+
+  /**
+   * The element (an ActionIcon) that this element will show in the right.
+   */
+  right: PropTypes.element,
+
+  /**
+   * Whether the logo to display is the white one (default: false).
+   */
+  whiteLogo: PropTypes.bool,
+};
+
+Header.defaultProps = {
+  left: null,
+  right: null,
+  whiteLogo: false,
+}
+
+export default Header;
--- react-app/src/components/Header/stories.js
+++ react-app/src/components/Header/stories.js
@@ -1,41 +1,56 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, boolean } from '@storybook/addon-knobs/react';
 
 import Header from './';
 import ActionIcon from 'components/ActionIcon';
 
 const stories = storiesOf('Header', module);
+stories.addDecorator(withKnobs);
+
+const backgroundStyle = {'background-color': 'black'};
 
 stories.add('Header with no side icons', () => (
-  <Header />
+  <div style={backgroundStyle}>
+    <Header whiteLogo={boolean('White logo', false)} />
+  </div>
 ));
 
 stories.add('Header with left icon', () => {
-  const leftIcon =
-    <ActionIcon
-      icon={ActionIcon.BACK}
-      onClick={action('Left ActionIcon clicked')}
-    />;
+  const leftIcon = <ActionIcon
+    icon={ActionIcon.SETTINGS}
+    onClick={action('Settings clicked')}
+  />
 
-  return <Header left={leftIcon} />
+  return (
+    <div style={backgroundStyle}>
+      <Header
+        left={leftIcon}
+        whiteLogo={boolean('White logo', false)}
+      />
+    </div>
+  );
 });
 
 stories.add('Header with left and right icons', () => {
-  const leftIcon =
-    <ActionIcon
-      icon={ActionIcon.BACK}
-      onClick={action('Left ActionIcon clicked')}
-    />;
-
-  const rightIcon =
-    <ActionIcon
-      icon={ActionIcon.REMOVE}
-      onClick={action('Right ActionIcon clicked')}
-    />;
-
-  return <Header
-    left={leftIcon}
-    right={rightIcon}
+  const leftIcon = <ActionIcon
+    icon={ActionIcon.BACK}
+    onClick={action('Back clicked')}
   />
+
+  const rightIcon = <ActionIcon
+    icon={ActionIcon.APPLY}
+    onClick={action('Apply clicked')}
+  />
+
+  return (
+    <div style={backgroundStyle}>
+      <Header
+        left={leftIcon}
+        right={rightIcon}
+        whiteLogo={boolean('White logo', false)}
+      />
+    </div>
+  );
 });
