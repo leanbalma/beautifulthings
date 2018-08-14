a3e870f0ca25bfbd85a66886d1d6ccc330c55212
Francisco Costa <francisco.costa@unixono.com>
2018-08-14 17:31:51 -0300

    Add wrappers to access local notifications functions

--- react-app/src/notifications/index.js
+++ react-app/src/notifications/index.js
@@ -1,4 +1,60 @@
 const DAILY = 1;
 const WEEKLY = 2;
 
-export { DAILY, WEEKLY }
+const SUNDAY_WEEKDAY = 7;
+
+function _showAddScreen() { /** TODO */ }
+
+function setNotifications(shcedule = DAILY) {
+  if (!window.cordova) return;
+
+  const notification = {
+    id: shcedule,
+    title: 'BeautifulThingsApp',
+    text: 'What\'s your beautiful thing today?',
+    icon: 'res://icon',
+    vibrate: true,
+    foreground: true,
+    trigger: {
+      every: {
+        hour: 22,
+        minute: 0,
+      },
+    }
+  }
+
+  if (shcedule === WEEKLY) notification.trigger.every.weekday = SUNDAY_WEEKDAY;
+
+  return new Promise(resolve => {
+    const callback = () => {
+      window.cordova.plugins.notification.local.on('click', _showAddScreen);
+      resolve();
+    }
+
+    clearNotifications()
+      .then(() => window.cordova.plugins.notification.local.schedule(notification, callback));
+  });
+}
+
+function clearNotifications() {
+  if (!window.cordova) return;
+
+  return new Promise(resolve => window.cordova.plugins.notification.local.cancelAll(resolve));
+}
+
+function getSchedule() {
+  if (!window.cordova) return true;
+
+  return new Promise(resolve => {
+    const processNotificationsArray = notifications => {
+      let scheduledNotificationId = null;
+      if (notifications.length) scheduledNotificationId = notifications[0];
+
+      resolve(scheduledNotificationId);
+    }
+
+    window.cordova.plugins.notification.local.getIds(processNotificationsArray);
+  });
+}
+
+export { setNotifications, clearNotifications, getSchedule, DAILY, WEEKLY }
