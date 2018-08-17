0aeaf87e27acff8747d49abc894158802d521598
Francisco Costa <francisco.costa@unixono.com>
2018-08-17 12:14:50 -0300

    Clear and move AppRouter from containers directory to src directory

---
+++ react-app/src/AppRouter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
+
+class AppRouter extends React.PureComponent {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default AppRouter;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from 'containers/AppRouter';
+import AppRouter from './AppRouter';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
