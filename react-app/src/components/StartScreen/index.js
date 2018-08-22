5a136e5d218a7117b2dcd2f414a6d5e86415d786
Francisco Costa <francisco.costa@unixono.com>
2018-08-22 17:56:33 -0300

    Improve StartScreen thinking in use it within a container

--- react-app/src/components/StartScreen/index.js
+++ react-app/src/components/StartScreen/index.js
@@ -5,12 +5,19 @@ import Account from 'account';
 
 import BaseUserPassScreen from 'components/BaseUserPassScreen';
 import Button from 'components/Button';
-import ButtonsModal from 'components/ButtonsModal';
 
 import styles from './index.module.scss';
 
 export default class StartScreen extends React.PureComponent {
   static propTypes = {
+    /**
+     * The function to call when sign in button is pressed
+     */
+    onSignIn: PropTypes.func.isRequired,
+
+    /**
+     * The function to call when sign up button is pressed
+     */
     onSignUp: PropTypes.func.isRequired,
   }
 
@@ -19,14 +26,9 @@ export default class StartScreen extends React.PureComponent {
     password: '',
     usernameError: '',
     passwordError: '',
-    isModalVisible: false,
   };
 
-  _validateForm() {
-    const { username, password } = this.state;
-
-    return username && password;
-  }
+  _validateForm = () => this.state.username && this.state.password;
 
   _handleUsernameChange = username => {
     const valid = Account.validateUsername(username);
@@ -66,71 +68,32 @@ export default class StartScreen extends React.PureComponent {
     if (validForm) this._signIn();
   }
 
-  _toggleModalVisibility = () => this.setState({ isModalVisible: !this.state.isModalVisible });
-
-  _handleSignUp = () => this.props.onSignUp();
-  _handleSignIn = () => this._signIn();
-
-  _signIn() { /* TODO: Show spinner, create account and signin or show modal */ }
-
-  _getSignInButton() {
-    const validFormData = this._validateForm();
-
-    return Button({
-      disabled: !validFormData,
-      onClick: this._handleSignIn,
-      children: "Sign in",
-    });
-  }
-
-  _signUpButton = Button({
-    onClick: this._handleSignUp,
-    children: "Sign up",
-  });
-
-  _closeModalButton = Button({
-    children: "Try again",
-    onClick: this._toggleModalVisibility,
-    small: true,
-  });
-
-  _getWrongUsernameOrPasswordModal() {
-    return ButtonsModal({
-      visible: this.state.isModalVisible,
-      message: "Your username or password is incorrect",
-      primaryButton: this._closeModalButton,
-    });
-  }
+  _signIn = () => this.props.onSignIn(this.state.username, this.state.password);
 
   render() {
     const { usernameError, passwordError } = this.state;
-
-    const signInButton = this._getSignInButton();
-    const wrongUsernameOrPasswordModal = this._getWrongUsernameOrPasswordModal();
+    const validForm = this._validateForm();
 
     return (
-      <div>
-        {wrongUsernameOrPasswordModal}
-        <BaseUserPassScreen
-          usernameError={usernameError}
-          passwordError={passwordError}
-          onUsernameChange={this._handleUsernameChange}
-          onPasswordChange={this._handlePasswordChange}
-          onPasswordEnter={this._handlePasswordEnter}
-        >
-          <div className={styles.container}>
-            <div>
-              {signInButton}
-            </div>
-            <div className={styles.createAccountLabelContainer}>
-              - Create your account -
-            </div>
-            <div>
-              {this._signUpButton}
-            </div>
+      <BaseUserPassScreen
+        usernameError={usernameError}
+        passwordError={passwordError}
+        onUsernameChange={this._handleUsernameChange}
+        onPasswordChange={this._handlePasswordChange}
+        onPasswordEnter={this._handlePasswordEnter}
+      >
+        <div className={styles.container}>
+          <Button onClick={this._signIn} disabled={!validForm}>
+            Sign in
+          </Button>
+          <div className={styles.createAccountLabelContainer}>
+            - Create your account -
           </div>
-        </BaseUserPassScreen>
-      </div>
+          <Button onClick={this.props.onSignUp}>
+            Sign up
+          </Button>
+        </div>
+      </BaseUserPassScreen>
     );
   }
 }
