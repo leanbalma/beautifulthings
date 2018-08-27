0574185743acffcc965b15d882bc2f393a1689da
Francisco Costa <francisco.costa@unixono.com>
2018-08-27 12:19:26 -0300

    Implement TODOs in StartScreenContainer

--- react-app/src/containers/StartScreenContainer.js
+++ react-app/src/containers/StartScreenContainer.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { signInAsync } from 'actions/account';
 
 import { changeHash, SCREENS_HASHES } from 'AppRouter';
+import { showAlertModal } from 'utils/alertModal';
 import { showLoadingModal, hideLoadingModal } from 'utils/spinner';
 
 import StartScreen from 'components/StartScreen';
@@ -16,9 +17,9 @@ const StartScreenContainer = ({ dispatch }) => {
       const signedIn = await signInAsync(username, password)(dispatch);
 
       if (signedIn) changeHash(SCREENS_HASHES.list);
-      else { /** TODO: Alert: Wrong username or password */ }
+      else showAlertModal('Wrong username or password');
     } catch (error) {
-      /** TODO: Alert: Cannot connect to server */
+      showAlertModal('Cannot connect to the server');
     } finally {
       hideLoadingModal();
     }
