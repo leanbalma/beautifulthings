fb16b3b17fd260cbb9b916d9c18f844d31f34404
Francisco Costa <francisco.costa@unixono.com>
2018-09-09 15:42:13 -0300

    Improve account to support symmetric encryption
    From now, the messages (entries) will be encrypted using a
    symmetric encryption schema with a random key generated when
    the user signs up. The public and secret keypair generated using
    the username and password (from now, the keypair) will be used
    to encrypt this key, which will be sent to the server and keep
    there.
    
    When the user signs in, the key must be requested and decrypted
    using the keypair, and the entries must be decrypted using this
    key. With this schema, it will be possible for the user to change
    te password and still be able to decrypt the entries.

--- react-app/src/account/test.js
+++ react-app/src/account/test.js
@@ -1,28 +1,51 @@
 /* eslint-env jest */
-import Account, { ErrorAuthenticationFail } from './';
-import naclUtils from 'tweetnacl-util';
+import Account from './';
 
 let account, keyPair;
 
 const mockUserData = {
   username:           '$/()RUFJ)wuwe84349',
   password:           'dnefu384(/·$873hf7g',
-  expectedPublicKey:  '6FUygceUHlKzGu/5ir4FNkmAtUF07uNeAAdp9+5hcy4=',
-  expectedSecretKey:  'm4jBZuDiCgcCz94VIWHmeVC9IsXOIavniA2pqeq5Gg0='
+  expectedPublicKey:  'QPnhimpSgSRC0leCAhAOdCXFIXiKIQurSCh4/j9GAho=',
+  expectedSecretKey:  'pLWFtUsKSQV6/5UmC2zYEtwbs8+0oNgyTYBDUqYg2Wo=',
 };
 
 test('match generated keypair with expected one', async () => {
   expect.assertions(2);
   keyPair = await Account.generateKeyPair(mockUserData.username, mockUserData.password);
 
-  const generatedPublicKey = naclUtils.encodeBase64(keyPair.publicKey);
+  const generatedPublicKey = Account.encodeBase64(keyPair.publicKey);
   expect(generatedPublicKey).toBe(mockUserData.expectedPublicKey);
 
-  const generatedSecretKey = naclUtils.encodeBase64(keyPair.secretKey);
+  const generatedSecretKey = Account.encodeBase64(keyPair.secretKey);
   expect(generatedSecretKey).toBe(mockUserData.expectedSecretKey);
 });
 
-test('success when encrypts and decrypts a message', () => {
+test('encrypt and decrypt the key', () => {
+  expect.assertions(1);
+  account = new Account(mockUserData.username, keyPair);
+
+  const key = account._key;
+  const encryptedKey = account._encryptWithKeyPair(key);
+  const encryptedKeyUTF8 = Account.encodeBase64(encryptedKey);
+  const decryptedKey = account.decryptWithKeyPair(encryptedKeyUTF8);
+
+  expect(key).toEqual(decryptedKey);
+});
+
+test('fail decrypting an invalid cipherkey', () => {
+  expect.assertions(1);
+  account = new Account(mockUserData.username, keyPair);
+
+  const key = account._key;
+  const encryptedKey = account._encryptWithKeyPair(key);
+  const encryptedKeyUTF8 = Account.encodeBase64(encryptedKey);
+  const modifiedEncryptedKeyUTF8 = `axs${encryptedKeyUTF8.substring(3)}`;
+
+  expect(() => account.decryptWithKeyPair(modifiedEncryptedKeyUTF8)).toThrow();
+});
+
+test('encrypt and decrypt a message', () => {
   expect.assertions(2);
   account = new Account(mockUserData.username, keyPair);
 
@@ -34,12 +57,12 @@ test('success when encrypts and decrypts a message', () => {
   expect(decryptedMessage).toBe(plainText);
 });
 
-test('fail when encrypts and decrypts a modified ciphertext', () => {
+test('fail decrypting an invalid ciphertext', () => {
   expect.assertions(2);
   const plainText = 'sómething with ñ and ü\n!_¡?\'{[^]}}à';
   const encryptedMessage = account.encrypt(plainText);
   expect(typeof encryptedMessage).toBe('string');
 
   const modifiedEncryptedMessage = `axs${encryptedMessage.substring(3)}`;
-  expect(() => account.decrypt(modifiedEncryptedMessage)).toThrow(ErrorAuthenticationFail);
+  expect(() => account.decrypt(modifiedEncryptedMessage)).toThrow();
 });
