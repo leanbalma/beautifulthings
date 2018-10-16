44950a9763b9fc514502af9264e090f08390324e
Francisco Costa <francisco.costa@unixono.com>
2018-10-16 07:22:01 -0300

    Comment the implementation of store resetting

--- react-app/src/reducers/index.js
+++ react-app/src/reducers/index.js
@@ -13,6 +13,14 @@ const appReducer = combineReducers({
 });
 
 const rootReducer = (state, action) => {
+  /**
+   * As the user's entries are preserved decrypted
+   * in the local store, it's important to clean it
+   * up when the user signs out for security reasons.
+   *
+   * For details about this implementation, take a look at:
+   * https://stackoverflow.com/questions/35622588/how-to-reset-the-state-of-a-redux-store
+   */
   if (action.type === SIGN_OUT) {
     state = undefined;
   }
